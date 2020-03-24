180beaa2aa1ea9d7d6005b20d8e8437d822f264e
Sarang8080 <ghodesarang9@gmail.com>
2020-03-24 10:32:19 +0530

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# Movie-Listing-Site
-Ability to search a movie/series with its name. Search is triggered with just typing on the placeholder. Ability to add movies to favorites and to remove from favorites as well. Users can see the movies that he/she added as favorites.
+# react-tmdb-app
---
+++ client/src/components/views/NavBar/Sections/LeftMenu.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Menu } from 'antd';
+
+
+function LeftMenu(props) {
+  return (
+    <Menu mode={props.mode}>
+      <Menu.Item key="favorite">
+        <a href="/favorite">Favorite</a>
+        
+      </Menu.Item>
+      <Menu.Item key="Search">
+      <a href="/search">Search Movies</a>
+      </Menu.Item>
+    </Menu>
+  )
+}
+
+export default LeftMenu
